a12d654588cea84e26385e235fcf8018fc8b33d4
tarzantrump3-collab <you@example.com>
2026-05-02 22:14:43 +0530

    cat, fork

---
+++ demo.c
@@ -0,0 +1,9 @@
+git pull
+git add .
+git commit -m "update demo.c"
+git push
+git clone --global https://github.com/tarzantrump3-collab/EOS
+git config --global user.name tarzantrump3-collab
+hii how are you
+git config user.email
+2nd attempt
